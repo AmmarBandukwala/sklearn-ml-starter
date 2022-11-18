ce25493b5bc0bdebec1c3f29f1431ff68d1126b3
Ammar <27972300+AmmarBandukwala@users.noreply.github.com>
2022-11-17 23:46:25 -0600

    drop features in scoring data

--- experiment/model_scoring.py
+++ experiment/model_scoring.py
@@ -21,11 +21,13 @@ def init():
     global data_path
     global model
     global date_column
+    global target_feature
     
     data_path = parameters["GENERAL"]["Data_Path"]
     model_name = parameters["GENERAL"]["Model_Name"]    
     date_column = parameters["PARAMETERS"]["Index"]
-
+    target_feature = parameters["PARAMETERS"]["Target"]
+    
     exp_runs = mlflow_helper.list_all_runs_for_experiment_by_name(experiment_name=model_name)
     run_id = mlflow_helper.get_run_id_lowest_rmse(exp_runs)
     model = mlflow_helper.get_model_by_run_id(run_id=run_id)
@@ -49,10 +51,17 @@ if __name__ == "__main__":
     
     # Load Data
     stock_data = StockData('Microsoft', pd.read_csv(filepath_or_buffer=os.path.join(data_path, 'Microsoft_5_Year_Historical.csv')))
-    raw_data = StockData.convert_currency_float(stock_data.Data)
-    raw_data[date_column] = pd.to_datetime(raw_data[date_column])
-    raw_data.set_index(date_column)
-    test_data = raw_data.copy()
+    test_data = StockData.convert_currency_float(stock_data.Data)
+    
+    test_data.loc[:, 'Yesterday_Close'] = test_data.loc[:, target_feature].shift()
+    test_data.loc[:, 'Yesterday_Diff'] = test_data.loc[:, 'Yesterday_Close'].diff()
+    test_data['Yesterday_Close'] = test_data['Yesterday_Close'].fillna(0)
+    test_data['Yesterday_Diff'] = test_data['Yesterday_Diff'].fillna(0)
+    
+    test_data = test_data.drop(date_column, axis=1)
+    test_data = test_data.drop(target_feature, axis=1)
+    
+    print(test_data.head())
 
     # Run Predict
     results = predict(test_data)
